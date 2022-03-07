876705c33b52b1103a8de5d74c937a2cf03fe42b
William Blair <60581672+williamblair333@users.noreply.github.com>
2022-03-07 13:00:03 -0500

    Add files via upload
    Moved functions into lib_folder for splitting path - file strings

--- lib_folder.py
+++ lib_folder.py
@@ -1,11 +1,45 @@
-import os
+import os, platform
 from pathlib import Path
+#################################################################################
+
+#Get the os we're working with
+os_type =  platform.platform()
+os_linux = "Linux"
+os_windows = "Windows"
+#################################################################################
+
+#split the keypath up to ssh directory and root directory variables
+#we can start changing directories and creating the dir_ssh
+#folder if it doesn't exist
+def folder_split(keypath):
+    if os_linux in os_type:
+        dir_split = keypath.split("/")
+        dir_ssh = dir_split[-1]
+        del dir_split[-1]
+        dir_root = ""
+
+        for element in dir_split:
+            dir_root += element + "/"
+
+    if os_windows in os_type:
+        dir_split = keypath.split("\\")
+        dir_ssh = dir_split[-1]
+        del dir_split[-1]
+        dir_root = ""
+
+        for element in dir_split:
+            dir_root += element + "\\"
+
+    return dir_root
+#################################################################################
 
 def folder_create(path_ssh, dir_ssh, dir_home):
     if os.path.exists(path_ssh):
         print(f'Folder "{dir_ssh}" exist!')
+        pass
     else:
         os.chdir(dir_home)
         os.mkdir(dir_ssh)
         print(f'Folder "{dir_ssh}" succesfully created!')
         return
+#################################################################################
--- ssh_keygen.py
+++ ssh_keygen.py
@@ -1,9 +1,10 @@
 ################################################################################
 #
 #READ THE COMMENTS BEFORE RUNNING
-#Run:        python3 ssh_keygen.py -b 4096 -k my_id_rsa -p /home/user/dir1/subdir1/subdir2/.ssh
+#Run:        python3 ssh_keygen.py -b 4096 -k my_id_rsa -p /home/user/.ssh
+#Run:        python3 ssh_keygen.py -b 4096 -k my_id_rsa -p C:\users\winuser\.ssh
 #File:       ssh_keygen.py
-#Date:       2022FEB02
+#Date:       2022FEB02, updated 2022MAR07
 #Author:     William Blair
 #Contact:	 williamblair333@gmail.com
 #Tested on:  Debian 11
@@ -13,9 +14,6 @@
 #- Generate ssh public and private keys compatible with Linux and hopefully
 #- other operating systems.
 #- TODO
-#- Make a function out of it for custom key type, strengths,
-#- file names, paths, and whatnot.
-#- Differentiate between OS versions
 #- Error handling..
 ################################################################################
 
@@ -35,24 +33,46 @@ from pathlib import Path
 from os import chmod
 #################################################################################
 
+#OS handling will go here.. function coming?
+os_type =  platform.platform()
+os_linux = "Linux"
+os_windows = "Windows"
+#################################################################################
+
 #Setting for variables for default arguments
 dir_home = str(Path.home())
 dir_ssh = ".ssh"
-path_ssh = dir_home + "/" + dir_ssh
+#################################################################################
 
 #Setting up arguments and defaults
+#Notes:  
+#    default is the value that the attribute gets when the argument is absent. 
+#    const is the value it gets when given. 
+#    const is allowed only when the action is store_const
 parser = argparse.ArgumentParser(description='To set ssh key bit size, name ' + \
 'and path location.')
 
-parser.add_argument('-b', '--bits', type=str, nargs='?', const="2048", \
+parser.add_argument('-b', '--bits', type=str, nargs='?', default="2048", \
 help='SSH key bit strength. Default is 2048.', required=False)
 
-parser.add_argument('-k', '--keyname',   type=str, nargs='?', const="id_rsa", \
+parser.add_argument('-k', '--keyname',   type=str, nargs='?', default="id_rsa", \
 help='Private key name. Default name is id_rsa.', required=False)
 
-parser.add_argument('-p', '--keypath',   type=str, nargs='?', const=path_ssh, \
-help='Private and public key folder path location.  Default path is $HOME/.ssh.', \
-required=False)
+if os_linux in os_type:
+    path_ssh = dir_home + "/" + dir_ssh
+    parser.add_argument('-p', '--keypath', \
+    type=str, nargs='?', \
+    default=path_ssh, \
+    help='Private and public key folder path location.  Default path is $HOME/.ssh.', \
+    required=False)
+
+if os_windows in os_type:
+    path_ssh = dir_home + "\\" + dir_ssh
+    parser.add_argument('-p', '--keypath', \
+    type=str, nargs='?', \
+    default=dir_home + "\.ssh", \
+    help='Private and public key folder path location.  Default path is $HOME/.ssh.', \
+    required=False)
 
 args = parser.parse_args()
 bits  = (args.bits)
@@ -60,44 +80,21 @@ keyname = (args.keyname)
 keypath = (args.keypath)
 #################################################################################
 
-#OS handling will go here.. function coming?
-os_type =  platform.platform()
-os_linux = "Linux"
-os_windows = "Windows"
-
-'''
-if os_windows in os_type:
-    print("This is windows")
-    parser.add_argument('-p', '--keypath', type=str, nargs='?', const="%USERPROFILE%\.ssh", \
-    help='Private and public key folder path location.  Default path is $HOME/.ssh.', \
-    required=False)
-else:
-    print("This is...")
-
-#keyname = "id_rsa"
-#sets a path variable
-#keypath = Path.cwd()
-#bits = 2048
-'''
-#################################################################################
-
 #split the keypath up to ssh directory and root directory variables
 #we can start changing directories and creating the dir_ssh
 #folder if it doesn't exist
-dir_split = keypath.split("/")
-dir_ssh = dir_split[-1]
-del dir_split[-1]
-dir_root = ""
-
-for element in dir_split:
-    dir_root += element + "/"
+dir_root = lib_folder.folder_split(keypath)
 
 #create the .ssh folder if it doesn't exist
 lib_folder.folder_create(keypath, dir_ssh, dir_root)
 os.chdir(keypath)
 #################################################################################
-
 #start private key generation
+if os.path.exists(keyname):
+    print("SSH key exists! Terminating program!")
+    quit()
+
+keyname = (keypath + "/" + keyname)
 keyname_public = keyname + '.pub'
 cipher = RSA.generate(int(bits))
 
@@ -105,8 +102,6 @@ with open(keyname, "wb") as file_private:
     file_private.write(cipher.exportKey('PEM'))
     file_private.close()
     #chmod can't find the file unless we use explicit path
-    keyname = (keypath + "/" + keyname)
-    print(keyname)
     chmod(keyname, 600)
 
 #start public key generation
