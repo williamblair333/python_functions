5615f6166ea18e22f52d5cc0f0cacac14f87feda
William Blair <60581672+williamblair333@users.noreply.github.com>
2022-03-07 14:27:35 -0500

    Add files via upload

--- lib_os.py
+++ lib_os.py
@@ -1,5 +1,11 @@
 import platform
 
+os_linux = "Linux"
+os_windows = "Windows"    
+
 def os_name():
     os_name = platform.platform()
     return print(os_name)
+
+def_os_ver():
+    os_ver = platform.version()
