7d902ecd07fbc82a60cb4fd23184f91f117492eb
William Blair <60581672+williamblair333@users.noreply.github.com>
2022-03-07 17:04:16 -0500

    Update ssh_keygen.py

--- ssh_keygen.py
+++ ssh_keygen.py
@@ -1,112 +1,34 @@
-################################################################################
-#
-#READ THE COMMENTS BEFORE RUNNING
-#Run:        python3 ssh_keygen.py -b 4096 -k my_id_rsa -p /home/user/.ssh
-#Run:        python3 ssh_keygen.py -b 4096 -k my_id_rsa -p C:\users\winuser\.ssh
-#File:       ssh_keygen.py
-#Date:       2022FEB02, updated 2022MAR07
-#Author:     William Blair
-#Contact:	 williamblair333@gmail.com
-#Tested on:  Debian 11, Windows 10 21H2,
-#
-#This script intends to do the following:
-#
-#- Generate ssh public and private keys compatible with Linux and hopefully
-#- other operating systems.
-#- TODO
-#- Error handling..
-################################################################################
-
-'''
-Old crypto libraries will cause problems. Run this..
-python3 -m pip uninstall crypto 
-python3 -m pip uninstall pycrypto 
-python3 -m pip install pycryptodome
-'''
-#################################################################################
-import lib_folder
-import lib_os
-import argparse, csv, os, platform, sys, time
-from subprocess import call
-from Crypto.PublicKey import RSA
+import os, platform
 from pathlib import Path
-from os import chmod
 #################################################################################
 
-#OS handling will go here.. function coming?
+#Get the os we're working with
 os_type =  platform.platform()
-os_ver = platform.version()
 os_linux = "Linux"
 os_windows = "Windows"
-
-#################################################################################
-
-#Setting for variables for default arguments
-dir_home = str(Path.home())
-dir_ssh = ".ssh"
-#################################################################################
-
-#Setting up arguments and defaults
-#Notes:  
-#    default is the value that the attribute gets when the argument is absent. 
-#    const is the value it gets when given. 
-#    const is allowed only when the action is store_const
-parser = argparse.ArgumentParser(description='To set ssh key bit size, name ' + \
-'and path location.')
-
-parser.add_argument('-b', '--bits', type=str, nargs='?', default="2048", \
-help='SSH key bit strength. Default is 2048.', required=False)
-
-parser.add_argument('-k', '--keyname',   type=str, nargs='?', default="id_rsa", \
-help='Private key name. Default name is id_rsa.', required=False)
-
-if os_linux in os_type:
-    path_ssh = dir_home + "/" + dir_ssh
-    parser.add_argument('-p', '--keypath', \
-    type=str, nargs='?', \
-    default=path_ssh, \
-    help='Private and public key folder path location.  Default path is $HOME/.ssh.', \
-    required=False)
-
-if os_windows in os_type:
-    path_ssh = dir_home + "\\" + dir_ssh
-    parser.add_argument('-p', '--keypath', \
-    type=str, nargs='?', \
-    default=dir_home + "\.ssh", \
-    help='Private and public key folder path location.  Default path is $HOME/.ssh.', \
-    required=False)
-
-args = parser.parse_args()
-bits  = (args.bits)
-keyname = (args.keyname)
-keypath = (args.keypath)
 #################################################################################
 
 #split the keypath up to ssh directory and root directory variables
 #we can start changing directories and creating the dir_ssh
 #folder if it doesn't exist
-dir_root = lib_folder.folder_split(keypath)
+def folder_split(keypath):
+    if os_linux in os_type:
+        dir_split = keypath.split("/")
+        dir_ssh = dir_split[-1]
+        del dir_split[-1]
+        dir_root = ""
 
-#create the .ssh folder if it doesn't exist
-lib_folder.folder_create(keypath, dir_ssh, dir_root)
-os.chdir(keypath)
+        for element in dir_split:
+            dir_root += element + "/"
+
+    if os_windows in os_type:
+        dir_split = keypath.split("\\")
+        dir_ssh = dir_split[-1]
+        del dir_split[-1]
+        dir_root = ""
+
+        for element in dir_split:
+            dir_root += element + "\\"
+
+    return dir_root
 #################################################################################
-#start private key generation
-if os.path.exists(keyname):
-    print("SSH key exists! Terminating program!")
-    quit()
-
-keyname = (keypath + "/" + keyname)
-keyname_public = keyname + '.pub'
-cipher = RSA.generate(int(bits))
-
-with open(keyname, "wb") as file_private:
-    file_private.write(cipher.exportKey('PEM'))
-    file_private.close()
-    #chmod can't find the file unless we use explicit path
-    chmod(keyname, 600)
-
-#start public key generation
-key_public = cipher.publickey()
-with open(keyname_public, "wb") as file_public:
-    file_public.write(key_public.exportKey('OpenSSH'))
